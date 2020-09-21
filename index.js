fafc5e49d8a92ef9e354cedf6137d1a472dd42ff
Yu Hao Wong <wongyuhao@gmail.com>
2020-09-21 17:05:28 +0800

    .

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 require('dotenv').config();
+const fs = require('fs')
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const { prefix, token } = require('./config.json');
+bot.commands = new Discord.Collection();
+bot.login(token);
 
-bot.login(TOKEN);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
+
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+bot.on('message', msg => {
+  if(!msg.content.startsWith(prefix)||msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  
+  if (!bot.commands.has(commandName)) return;
+
+  try {
+  	bot.commands.get(commandName).execute(msg, args);
+  } catch (error) {
+	  console.error(error);
+	  bot.reply('there was an error trying to execute that command!');
   }
+
 });

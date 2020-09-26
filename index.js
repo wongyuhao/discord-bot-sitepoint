84401423255a3ecd4b5004f94ccc0cdb42e02ab3
Yu Hao Wong <wongyuhao@gmail.com>
2020-09-27 01:03:33 +0800

    test alias implementation

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ require('dotenv').config();
 const fs = require('fs')
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const { prefix} = require('./config.json');
-bot.commands = new Discord.Collection();
+const {prefix} = require('./config.json');
+
 const TOKEN = process.env.BOT_TOKEN
 bot.login(TOKEN);
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+  bot.commands.set(command.name, command);
+  if(command.aliases){
+  command.aliases.forEach(
+    alias => bot.aliases.set(alias, command.name)
+  )
+  }else{
+    console.log(`Command ${command.name} has no aliases.` )
+  }
 }
 
 
@@ -26,13 +36,24 @@ bot.on('message', msg => {
   const args = msg.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
   
-  if (!bot.commands.has(commandName)) return;
-
-  try {
-  	bot.commands.get(commandName).execute(msg, args);
-  } catch (error) {
-	  console.error(error);
-	  bot.reply('there was an error trying to execute that command!');
+  if (bot.commands.has(commandName)){
+    try {
+      bot.commands.get(commandName).execute(msg, args);
+    } catch (error) {
+      console.error(error);
+      bot.reply('there was an error trying to execute that command!');
+    }
+  }else if(bot.aliases.has(commandName)){
+    try {
+      bot.commands.get(bot.aliases.get(commandName)).execute(msg, args);
+    } catch (error) {
+      console.error(error);
+      bot.reply('there was an error trying to execute that command!');
+    }
+  }else{
+    return;
   }
+
+  
 
 });
